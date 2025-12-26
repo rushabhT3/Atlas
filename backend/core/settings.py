512b47e13bda057e4c3d8fd7c66d329096c72d74
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2025-12-26 13:47:30 +0530

    domain: update settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,11 +25,16 @@ SECRET_KEY = "django-insecure-+#-cvht_2h=iphs-p=xeczvadhk)q10kvwn*os2#b89*6e+@y2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://atlas-rho-sable.vercel.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://atlas-rho-sable.vercel.app",
 ]
 
 # Application definition
